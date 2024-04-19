59ab2ccda50015401638f44be63defa2aa9733fa
Francisco Lisboa <francisco.lisboa@poatek.com>
2024-04-19 11:32:00 -0300

    init list screen test

---
+++ app/src/androidTest/java/com/example/pokedexapp/ui/pokemon_list_screen/PokemonListScreenKtTest.kt
@@ -0,0 +1,19 @@
+package com.example.pokedexapp.ui.pokemon_list_screen
+
+import androidx.compose.ui.test.junit4.createComposeRule
+import org.junit.Rule
+import org.junit.Test
+
+class PokemonListScreenKtTest {
+
+    @get:Rule
+    val composeTestRule = createComposeRule()
+
+    @Test
+    fun navigateToDetails(){
+
+        composeTestRule.setContent {
+            
+        }
+    }
+}
