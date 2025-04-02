ca9431e513ec9e20bd5fba6ce1e0dc6b334b8cb2
Francisco Lisboa <francisco.lisboa@poatek.com>
2025-04-02 16:24:44 -0300

    added request and image fetch

--- app/src/main/java/com/example/pokedexapp/ui/notifications/DailyNotificationWorker.kt
+++ app/src/main/java/com/example/pokedexapp/ui/notifications/DailyNotificationWorker.kt
@@ -1,39 +1,67 @@
 package com.example.pokedexapp.ui.notifications
 
 import android.content.Context
+import androidx.glance.appwidget.updateAll
 import androidx.hilt.work.HiltWorker
 import androidx.work.CoroutineWorker
 import androidx.work.WorkerParameters
+import com.example.pokedexapp.domain.repository.PokemonRepository
 import com.example.pokedexapp.domain.use_cases.RandomPokemonUseCase
 import com.example.pokedexapp.domain.utils.Response
+import com.example.pokedexapp.ui.widgets.daily_pokemon.DailyPokemonWidget
+import com.example.pokedexapp.ui.widgets.daily_pokemon.DailyPokemonWidgetState
+import com.example.pokedexapp.ui.widgets.daily_pokemon.DailyPokemonWidgetStateManager
 import dagger.assisted.Assisted
 import dagger.assisted.AssistedInject
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
 
 @HiltWorker
 class DailyNotificationWorker
-    @AssistedInject
-    constructor(
-        @Assisted
-        private val appContext: Context,
-        @Assisted
-        private val params: WorkerParameters,
-        private val randomPokemonUseCase: RandomPokemonUseCase,
-    ) : CoroutineWorker(appContext, params) {
-        override suspend fun doWork(): Result {
-            val notificationPokemonResponse = randomPokemonUseCase.getRandomPokemonMinimalInfo()
+@AssistedInject
+constructor(
+    @Assisted
+    private val appContext: Context,
+    @Assisted
+    private val params: WorkerParameters,
+    private val randomPokemonUseCase: RandomPokemonUseCase,
+    private val pokemonRepository: PokemonRepository
+) : CoroutineWorker(appContext, params) {
+    override suspend fun doWork(): Result {
+        val notificationPokemonResponse = randomPokemonUseCase.getRandomPokemonMinimalInfo()
 
-            when (notificationPokemonResponse) {
-                is Response.Error -> {
-                    return Result.failure()
+        when (notificationPokemonResponse) {
+            is Response.Error -> {
+                return Result.failure()
+            }
+
+            is Response.Success -> {
+                withContext(Dispatchers.IO) {
+                    val pokemonDetails =
+                        pokemonRepository.getPokemonDetailById(notificationPokemonResponse.data.id)
+
+                    if (pokemonDetails is Response.Success) {
+
+                        DailyPokemonWidgetStateManager.state = DailyPokemonWidgetState(
+                            pokemonDetails.data.id,
+                            pokemonDetails.data.name,
+                            pokemonDetails.data.frontDefaultSprite.spriteUrl,
+                            pokemonDetails.data.primaryType,
+                            pokemonDetails.data.secondaryType,
+                        )
+
+                        DailyPokemonWidget().updateAll(appContext)
+                    }
+
+                    DailyPokemonWidget().updateAll(context = appContext)
                 }
 
-                is Response.Success -> {
-                    DailyPokemonNotification(appContext).showNotification(
-                        pokemonId = notificationPokemonResponse.data.id,
-                        pokemonName = notificationPokemonResponse.data.name,
-                    )
-                    return Result.success()
-                }
+                DailyPokemonNotification(appContext).showNotification(
+                    pokemonId = notificationPokemonResponse.data.id,
+                    pokemonName = notificationPokemonResponse.data.name,
+                )
+                return Result.success()
             }
         }
     }
+}
--- app/src/main/java/com/example/pokedexapp/ui/widgets/daily_pokemon/DailyPokemonWidget.kt
+++ app/src/main/java/com/example/pokedexapp/ui/widgets/daily_pokemon/DailyPokemonWidget.kt
@@ -1,19 +1,77 @@
 package com.example.pokedexapp.ui.widgets.daily_pokemon
 
 import android.content.Context
-import androidx.compose.ui.graphics.Color
+import android.graphics.Bitmap
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.core.graphics.drawable.toBitmapOrNull
 import androidx.glance.GlanceId
-import androidx.glance.GlanceModifier
+import androidx.glance.Image
+import androidx.glance.ImageProvider
 import androidx.glance.appwidget.GlanceAppWidget
 import androidx.glance.appwidget.provideContent
-import androidx.glance.background
-import androidx.glance.layout.Box
-import androidx.glance.layout.fillMaxSize
+import coil.imageLoader
+import coil.request.CachePolicy
+import coil.request.ErrorResult
+import coil.request.ImageRequest
+import coil.request.SuccessResult
+import com.example.pokedexapp.R
+import com.example.pokedexapp.domain.models.PokemonTypes
 
-class DailyPokemonWidget: GlanceAppWidget() {
+
+class DailyPokemonWidget : GlanceAppWidget() {
     override suspend fun provideGlance(context: Context, id: GlanceId) {
+        val state = DailyPokemonWidgetStateManager.state
+
         provideContent {
-            Box(GlanceModifier.fillMaxSize().background(Color.Red)) {  }
+            state?.let { state ->
+                var imageBitmap by remember {
+                    mutableStateOf<Bitmap?>(null)
+                }
+
+                LaunchedEffect(state.imageUrl) {
+                    imageBitmap = getImageBitmap(context, state.imageUrl)
+                }
+
+                imageBitmap?.let {
+                    Image(
+                        provider = ImageProvider(it),
+                        contentDescription = null
+                    )
+                } ?: run {
+                    Image(
+                        provider = ImageProvider(R.drawable.baseline_no_photography_24),
+                        contentDescription = null
+                    )
+                }
+            }
+        }
+    }
+
+    private suspend fun getImageBitmap(context: Context, url: String?): Bitmap? {
+        val request = ImageRequest.Builder(context).data(url).apply {
+            memoryCachePolicy(CachePolicy.DISABLED)
+            diskCachePolicy(CachePolicy.DISABLED)
+        }.build()
+
+        return when (val result = context.imageLoader.execute(request)) {
+            is ErrorResult -> null
+            is SuccessResult -> result.drawable.toBitmapOrNull()
         }
     }
 }
+
+object DailyPokemonWidgetStateManager {
+    var state: DailyPokemonWidgetState? = null
+}
+
+data class DailyPokemonWidgetState(
+    val id: String,
+    val name: String,
+    val imageUrl: String?,
+    val primaryType: PokemonTypes,
+    val secondaryType: PokemonTypes?
+)
