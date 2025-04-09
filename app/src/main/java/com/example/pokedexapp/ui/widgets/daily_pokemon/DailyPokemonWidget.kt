5d5d2cd3383c052a0411492a2161d68848b2e072
Francisco Lisboa <francisco.lisboa@poatek.com>
2025-04-09 11:49:18 -0300

    when clicking widget will go to pokemons details

--- app/src/main/java/com/example/pokedexapp/ui/widgets/daily_pokemon/DailyPokemonWidget.kt
+++ app/src/main/java/com/example/pokedexapp/ui/widgets/daily_pokemon/DailyPokemonWidget.kt
@@ -13,21 +13,27 @@ import androidx.glance.GlanceId
 import androidx.glance.GlanceModifier
 import androidx.glance.Image
 import androidx.glance.ImageProvider
+import androidx.glance.action.ActionParameters
+import androidx.glance.action.actionParametersOf
+import androidx.glance.action.actionStartActivity
+import androidx.glance.action.clickable
 import androidx.glance.appwidget.GlanceAppWidget
 import androidx.glance.appwidget.provideContent
 import androidx.glance.background
 import androidx.glance.layout.Alignment
 import androidx.glance.layout.Box
 import androidx.glance.layout.fillMaxSize
-import androidx.glance.layout.fillMaxWidth
 import androidx.glance.layout.size
 import coil.imageLoader
 import coil.request.CachePolicy
 import coil.request.ErrorResult
 import coil.request.ImageRequest
 import coil.request.SuccessResult
+import com.example.pokedexapp.MainActivity
 import com.example.pokedexapp.R
 import com.example.pokedexapp.domain.use_cases.DailyPokemonWidgetUseCase
+import com.example.pokedexapp.ui.navigation.Screen
+import com.example.pokedexapp.ui.utils.INTENT_EXTRA_DEEPLINK_KEY
 import dagger.hilt.EntryPoint
 import dagger.hilt.EntryPoints
 import dagger.hilt.InstallIn
@@ -47,7 +53,12 @@ class DailyPokemonWidget : GlanceAppWidget() {
                 contentAlignment = Alignment.Center,
                 modifier =
                     GlanceModifier.fillMaxSize()
-                        .background(dailyPokemonState.primaryType.color),
+                        .background(dailyPokemonState.primaryType.color)
+                        .clickable(actionStartActivity<MainActivity>(
+                            parameters = actionParametersOf(
+                                ActionParameters.Key<String>(INTENT_EXTRA_DEEPLINK_KEY) to Screen.PokemonDetailScreen.makeDeeplink(pokemonId = dailyPokemonState.id))
+                        ))
+                ,
             ) {
                 var imageBitmap by remember {
                     mutableStateOf<Bitmap?>(null)
@@ -61,7 +72,7 @@ class DailyPokemonWidget : GlanceAppWidget() {
                     Image(
                         modifier =
                             GlanceModifier
-                                .fillMaxWidth(),
+                                .fillMaxSize(),
                         provider = ImageProvider(it),
                         contentDescription = null,
                     )
