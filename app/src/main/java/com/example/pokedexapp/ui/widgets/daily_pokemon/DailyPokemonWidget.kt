ea3915070aa9527ae45ca827d3720289f3b86300
Francisco Lisboa <francisco.lisboa@poatek.com>
2025-04-09 12:11:33 -0300

    Separated content from widget

--- app/src/main/java/com/example/pokedexapp/ui/widgets/daily_pokemon/DailyPokemonWidget.kt
+++ app/src/main/java/com/example/pokedexapp/ui/widgets/daily_pokemon/DailyPokemonWidget.kt
@@ -7,33 +7,18 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
-import androidx.compose.ui.unit.dp
 import androidx.core.graphics.drawable.toBitmapOrNull
 import androidx.glance.GlanceId
 import androidx.glance.GlanceModifier
-import androidx.glance.Image
-import androidx.glance.ImageProvider
-import androidx.glance.action.ActionParameters
-import androidx.glance.action.actionParametersOf
-import androidx.glance.action.actionStartActivity
-import androidx.glance.action.clickable
 import androidx.glance.appwidget.GlanceAppWidget
 import androidx.glance.appwidget.provideContent
-import androidx.glance.background
-import androidx.glance.layout.Alignment
-import androidx.glance.layout.Box
-import androidx.glance.layout.fillMaxSize
-import androidx.glance.layout.size
 import coil.imageLoader
 import coil.request.CachePolicy
 import coil.request.ErrorResult
 import coil.request.ImageRequest
 import coil.request.SuccessResult
-import com.example.pokedexapp.MainActivity
-import com.example.pokedexapp.R
 import com.example.pokedexapp.domain.use_cases.DailyPokemonWidgetUseCase
-import com.example.pokedexapp.ui.navigation.Screen
-import com.example.pokedexapp.ui.utils.INTENT_EXTRA_DEEPLINK_KEY
+import com.example.pokedexapp.ui.widgets.PokemonWidgetContent
 import dagger.hilt.EntryPoint
 import dagger.hilt.EntryPoints
 import dagger.hilt.InstallIn
@@ -44,46 +29,26 @@ class DailyPokemonWidget : GlanceAppWidget() {
         context: Context,
         id: GlanceId,
     ) {
-        val dailyPokemonWidgetUseCase: DailyPokemonWidgetUseCase = DailyPokemonWidgetUseCaseEntrypoint.get(context)
+        val dailyPokemonWidgetUseCase: DailyPokemonWidgetUseCase =
+            DailyPokemonWidgetUseCaseEntrypoint.get(context)
 
         val dailyPokemonState = dailyPokemonWidgetUseCase.getDailyPokemon()
 
         provideContent {
-            Box(
-                contentAlignment = Alignment.Center,
-                modifier =
-                    GlanceModifier.fillMaxSize()
-                        .background(dailyPokemonState.primaryType.color)
-                        .clickable(actionStartActivity<MainActivity>(
-                            parameters = actionParametersOf(
-                                ActionParameters.Key<String>(INTENT_EXTRA_DEEPLINK_KEY) to Screen.PokemonDetailScreen.makeDeeplink(pokemonId = dailyPokemonState.id))
-                        ))
-                ,
-            ) {
-                var imageBitmap by remember {
-                    mutableStateOf<Bitmap?>(null)
-                }
-
-                LaunchedEffect(dailyPokemonState.imageUrl) {
-                    imageBitmap = getImageBitmap(context, dailyPokemonState.imageUrl)
-                }
-
-                imageBitmap?.let {
-                    Image(
-                        modifier =
-                            GlanceModifier
-                                .fillMaxSize(),
-                        provider = ImageProvider(it),
-                        contentDescription = null,
-                    )
-                } ?: run {
-                    Image(
-                        modifier = GlanceModifier.size(48.dp),
-                        provider = ImageProvider(R.drawable.baseline_no_photography_24),
-                        contentDescription = null,
-                    )
-                }
+            var imageBitmap by remember {
+                mutableStateOf<Bitmap?>(null)
             }
+
+            LaunchedEffect(dailyPokemonState.imageUrl) {
+                imageBitmap = getImageBitmap(context, dailyPokemonState.imageUrl)
+            }
+
+            PokemonWidgetContent(
+                GlanceModifier,
+                dailyPokemonState.id,
+                imageBitmap,
+                dailyPokemonState.primaryType
+            )
         }
     }
 }
